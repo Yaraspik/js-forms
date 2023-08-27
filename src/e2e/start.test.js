9de5f6747ff4eb1d2338c19380b5eb5e71d24cb6
Stepan Mareev <yaraspik@ya.ru>
2023-08-27 19:13:23 +0300

    Init

--- src/e2e/start.test.js
+++ src/e2e/start.test.js
@@ -1,15 +1,18 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import puppeteer from 'puppeteer';
+import { fork } from 'child_process';
 
 describe('Page start', () => {
   let browser;
   let page;
+  let server;
 
   beforeEach(async () => {
+    server = fork(`${__dirname}/e2e.server.js`);
     browser = await puppeteer.launch({
-      headless: false,
-      slowMo: 100,
-      devtools: true,
+      // headless: false,
+      // slowMo: 100,
+      // devtools: true,
     });
 
     page = await browser.newPage();
@@ -26,5 +29,6 @@ describe('Page start', () => {
 
   afterEach(async () => {
     await browser.close();
+    await server.kill();
   });
 });
